4343f6723f717e645a3da5ab0739ee38ccb03ced
Jordan M <itdev@naturamedicatrix.lu>
2025-06-11 16:40:55 +0200

    feat: ajout des champs personnalisés amazon

--- modules/champ_personnalise_produit/champ_personnalise_produit.php
+++ modules/champ_personnalise_produit/champ_personnalise_produit.php
@@ -74,6 +74,45 @@ class Champ_Personnalise_Produit extends Module
             $result = $result && Db::getInstance()->execute($sql3);
         }
         
+        // Ajoute la colonne nm_days à la table product
+        $sql4 = 'ALTER TABLE `' . _DB_PREFIX_ . 'product` ADD COLUMN `nm_days` SMALLINT(5) NULL AFTER `dlu_checkbox`;';
+        
+        // Vérifie si la colonne nm_days existe déjà
+        $columnNmDaysExists = Db::getInstance()->executeS(
+            'SHOW COLUMNS FROM `' . _DB_PREFIX_ . 'product` LIKE "nm_days"'
+        );
+        
+        // Exécute la requête SQL seulement si la colonne n'existe pas
+        if (empty($columnNmDaysExists)) {
+            $result = $result && Db::getInstance()->execute($sql4);
+        }
+        
+        // Ajoute la colonne amazon à la table product
+        $sql5 = 'ALTER TABLE `' . _DB_PREFIX_ . 'product` ADD COLUMN `amazon` TEXT NULL AFTER `nm_days`;';
+        
+        // Vérifie si la colonne amazon existe déjà
+        $columnAmazonExists = Db::getInstance()->executeS(
+            'SHOW COLUMNS FROM `' . _DB_PREFIX_ . 'product` LIKE "amazon"'
+        );
+        
+        // Exécute la requête SQL seulement si la colonne n'existe pas
+        if (empty($columnAmazonExists)) {
+            $result = $result && Db::getInstance()->execute($sql5);
+        }
+        
+        // Ajoute la colonne amazon_be à la table product
+        $sql6 = 'ALTER TABLE `' . _DB_PREFIX_ . 'product` ADD COLUMN `amazon_be` TEXT NULL AFTER `amazon`;';
+        
+        // Vérifie si la colonne amazon_be existe déjà
+        $columnAmazonBeExists = Db::getInstance()->executeS(
+            'SHOW COLUMNS FROM `' . _DB_PREFIX_ . 'product` LIKE "amazon_be"'
+        );
+        
+        // Exécute la requête SQL seulement si la colonne n'existe pas
+        if (empty($columnAmazonBeExists)) {
+            $result = $result && Db::getInstance()->execute($sql6);
+        }
+        
         return $result && 
                parent::install() && 
                $this->registerHook([
@@ -112,8 +151,11 @@ class Champ_Personnalise_Produit extends Module
         $lot = '';
         $dlu = '';
         $dluCheckbox = false;
+        $nmDays = null;
+        $amazon = '';
+        $amazonBe = '';
         $result = Db::getInstance()->getRow(
-            'SELECT lot, dlu, dlu_checkbox FROM `' . _DB_PREFIX_ . 'product` WHERE id_product = ' . (int) $productId
+            'SELECT lot, dlu, dlu_checkbox, nm_days, amazon, amazon_be FROM `' . _DB_PREFIX_ . 'product` WHERE id_product = ' . (int) $productId
         );
         
         if ($result) {
@@ -126,6 +168,15 @@ class Champ_Personnalise_Produit extends Module
             if (isset($result['dlu_checkbox'])) {
                 $dluCheckbox = (bool)$result['dlu_checkbox'];
             }
+            if (isset($result['nm_days'])) {
+                $nmDays = (int)$result['nm_days'];
+            }
+            if (isset($result['amazon'])) {
+                $amazon = $result['amazon'];
+            }
+            if (isset($result['amazon_be'])) {
+                $amazonBe = $result['amazon_be'];
+            }
         }
         
         // Onglet "details"
@@ -133,6 +184,52 @@ class Champ_Personnalise_Produit extends Module
             return;
         }
         $detailsTabFormBuilder = $formBuilder->get('details');
+        
+        // Crée un nouveau groupe pour Amazon s'il n'existe pas déjà
+        if (!$detailsTabFormBuilder->has('amazon_links')) {
+            $detailsTabFormBuilder->add('amazon_links', 'Symfony\\Component\\Form\\Extension\\Core\\Type\\FormType', [
+                'label' => 'Amazon',
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-group',
+                    'id' => 'product_details_amazon_links'
+                ],
+                'label_attr' => [
+                    'class' => 'form-control-label h3',
+                    'style' => 'margin-bottom: 1rem;'
+                ],
+            ]);
+        }
+        
+        // Récupère le groupe amazon_links
+        $amazonGroup = $detailsTabFormBuilder->get('amazon_links');
+        
+        // Ajoute les champs amazon et amazon_be
+        $amazonGroup->add('amazon', 'Symfony\\Component\\Form\\Extension\\Core\\Type\\TextType', [
+            'label' => 'Bouton Amazon',
+            'required' => false,
+            'attr' => [
+                'placeholder' => 'https://www.amazon.fr/...',
+                'class' => 'form-control',
+            ],
+            'label_attr' => [
+                'style' => 'display: inline-block; background-color: orange; padding: 3px 8px; border-radius: 3px; color: black;',
+            ],
+            'data' => $amazon,
+        ]);
+        
+        $amazonGroup->add('amazon_be', 'Symfony\\Component\\Form\\Extension\\Core\\Type\\TextType', [
+            'label' => 'Bouton Amazon (be)',
+            'required' => false,
+            'attr' => [
+                'placeholder' => 'https://www.amazon.com.be/...',
+                'class' => 'form-control',
+            ],
+            'label_attr' => [
+                'style' => 'display: inline-block; background-color: orange; padding: 3px 8px; border-radius: 3px; color: black;',
+            ],
+            'data' => $amazonBe,
+        ]);
         
         // Cible le block "group-form" references
         if ($detailsTabFormBuilder->has('references')) {
@@ -143,7 +240,7 @@ class Champ_Personnalise_Produit extends Module
                 'label' => 'Numéro de lot en cours',
                 'required' => false,
                 'attr' => [
-                    'placeholder' => 'Saisissez le numéro de lot en cours',
+                    'placeholder' => '',
                     'class' => 'form-control',
                 ],
                 'data' => $lot,
@@ -169,6 +266,18 @@ class Champ_Personnalise_Produit extends Module
                 'label' => $this->l('DLU courte'),
                 'required' => false,
                 'data' => $dluCheckbox,
+            ]);
+            
+            // Ajout du champ jours de prise du produit
+            $referencesGroup->add('nm_days', 'Symfony\\Component\\Form\\Extension\\Core\\Type\\NumberType', [
+                'label' => $this->l('Jours de prise du produit (en jours)'),
+                'required' => false,
+                'attr' => [
+                    'placeholder' => '',
+                    'class' => 'form-control',
+                    'min' => 0
+                ],
+                'data' => $nmDays,
             ]);
         } else {
             // Fallback si le groupe de références n'existe pas
@@ -224,23 +333,21 @@ class Champ_Personnalise_Produit extends Module
                 ]
             );
             
-            // Ajoute le champ DLU juste après le champ lot
+            // Ajoute le champ nm_days juste après dlu_checkbox
             $formBuilderModifier->addAfter(
                 $detailsTabFormBuilder,
-                'lot',
-                'dlu',
-                'Symfony\\Component\\Form\\Extension\\Core\\Type\\DateType',
+                'dlu_checkbox',
+                'nm_days',
+                'Symfony\\Component\\Form\\Extension\\Core\\Type\\NumberType',
                 [
-                    'label' => 'DLU',
+                    'label' => $this->l('Jours de prise du produit (en jours)'),
                     'required' => false,
-                    'widget' => 'single_text',
-                    'format' => 'yyyy-MM-dd',
-                    'html5' => true,
                     'attr' => [
-                        'placeholder' => 'AAAA-MM-JJ',
+                        'placeholder' => 'Nombre de jours',
                         'class' => 'form-control',
+                        'min' => 0
                     ],
-                    'data' => !empty($dlu) ? new \DateTime($dlu) : null,
+                    'data' => $nmDays,
                 ]
             );
         }
@@ -293,6 +400,42 @@ class Champ_Personnalise_Produit extends Module
             $dluCheckbox = (int)!empty($_POST['dlu_checkbox']);
         }
         
+        // Vérifie les différents endroits possibles pour nm_days
+        $nmDays = 'NULL';
+        if (isset($_POST['product']['details']['references']['nm_days']) && $_POST['product']['details']['references']['nm_days'] !== '') {
+            $nmDays = (int)$_POST['product']['details']['references']['nm_days'];
+        } elseif (isset($_POST['product']['details']['nm_days']) && $_POST['product']['details']['nm_days'] !== '') {
+            $nmDays = (int)$_POST['product']['details']['nm_days'];
+        } elseif (isset($_POST['details']['references']['nm_days']) && $_POST['details']['references']['nm_days'] !== '') {
+            $nmDays = (int)$_POST['details']['references']['nm_days'];
+        } elseif (isset($_POST['nm_days']) && $_POST['nm_days'] !== '') {
+            $nmDays = (int)$_POST['nm_days'];
+        }
+        
+        // Vérifie les différents endroits possibles pour amazon
+        $amazon = '';
+        if (isset($_POST['product']['details']['amazon_links']['amazon'])) {
+            $amazon = pSQL($_POST['product']['details']['amazon_links']['amazon']);
+        } elseif (isset($_POST['product']['details']['amazon'])) {
+            $amazon = pSQL($_POST['product']['details']['amazon']);
+        } elseif (isset($_POST['details']['amazon_links']['amazon'])) {
+            $amazon = pSQL($_POST['details']['amazon_links']['amazon']);
+        } elseif (isset($_POST['amazon'])) {
+            $amazon = pSQL($_POST['amazon']);
+        }
+        
+        // Vérifie les différents endroits possibles pour amazon_be
+        $amazonBe = '';
+        if (isset($_POST['product']['details']['amazon_links']['amazon_be'])) {
+            $amazonBe = pSQL($_POST['product']['details']['amazon_links']['amazon_be']);
+        } elseif (isset($_POST['product']['details']['amazon_be'])) {
+            $amazonBe = pSQL($_POST['product']['details']['amazon_be']);
+        } elseif (isset($_POST['details']['amazon_links']['amazon_be'])) {
+            $amazonBe = pSQL($_POST['details']['amazon_links']['amazon_be']);
+        } elseif (isset($_POST['amazon_be'])) {
+            $amazonBe = pSQL($_POST['amazon_be']);
+        }
+        
         // Formate la date si elle existe
         $dluFormatted = 'NULL';
         if (!empty($dlu)) {
@@ -304,7 +447,10 @@ class Champ_Personnalise_Produit extends Module
             'UPDATE `' . _DB_PREFIX_ . 'product` 
             SET lot = "' . $lot . '", 
                 dlu = ' . $dluFormatted . ',
-                dlu_checkbox = ' . $dluCheckbox . ' 
+                dlu_checkbox = ' . $dluCheckbox . ',
+                nm_days = ' . $nmDays . ',
+                amazon = "' . $amazon . '",
+                amazon_be = "' . $amazonBe . '" 
             WHERE id_product = ' . $productId
         );
     }
@@ -320,14 +466,14 @@ class Champ_Personnalise_Produit extends Module
         
         $productId = (int) $params['id_product'];
         
-        // Récupère les données de lot et DLU depuis la table product
+        // Récupère les données de lot, DLU et autres champs depuis la table product
         $result = Db::getInstance()->getRow(
-            'SELECT lot, dlu, dlu_checkbox FROM `' . _DB_PREFIX_ . 'product` WHERE id_product = ' . $productId
+            'SELECT lot, dlu, dlu_checkbox, nm_days, amazon, amazon_be FROM `' . _DB_PREFIX_ . 'product` WHERE id_product = ' . $productId
         );
         
         if ($result) {
             // Debug pour vérifier les données récupérées
-            PrestaShopLogger::addLog('Données produit ID '.$productId.': lot='.(isset($result['lot']) ? $result['lot'] : 'non défini').', dlu='.(isset($result['dlu']) ? $result['dlu'] : 'non défini').', dlu_checkbox='.(isset($result['dlu_checkbox']) ? $result['dlu_checkbox'] : 'non défini'), 1);
+            PrestaShopLogger::addLog('Données produit ID '.$productId.': lot='.(isset($result['lot']) ? $result['lot'] : 'non défini').', dlu='.(isset($result['dlu']) ? $result['dlu'] : 'non défini').', dlu_checkbox='.(isset($result['dlu_checkbox']) ? $result['dlu_checkbox'] : 'non défini').', nm_days='.(isset($result['nm_days']) ? $result['nm_days'] : 'non défini').', amazon='.(isset($result['amazon']) ? 'présent' : 'non défini').', amazon_be='.(isset($result['amazon_be']) ? 'présent' : 'non défini'), 1);
             
             // Ajouter les champs personnalisés à l'objet produit
             if (isset($result['lot'])) {
@@ -340,6 +486,18 @@ class Champ_Personnalise_Produit extends Module
             
             if (isset($result['dlu_checkbox'])) {
                 $params['product']['dlu_checkbox'] = $result['dlu_checkbox'];
+            }
+            
+            if (isset($result['nm_days'])) {
+                $params['product']['nm_days'] = $result['nm_days'];
+            }
+            
+            if (isset($result['amazon'])) {
+                $params['product']['amazon'] = $result['amazon'];
+            }
+            
+            if (isset($result['amazon_be'])) {
+                $params['product']['amazon_be'] = $result['amazon_be'];
             }
         }
         
@@ -364,9 +522,9 @@ class Champ_Personnalise_Produit extends Module
             
             if (!empty($products)) {
                 foreach ($products as &$product) {
-                    // Récupère les infos lot et DLU depuis la base
+                    // Récupère les infos lot, DLU et autres champs depuis la base
                     $result = Db::getInstance()->getRow(
-                        'SELECT lot, dlu, dlu_checkbox FROM `' . _DB_PREFIX_ . 'product` WHERE id_product = ' . (int)$product['id_product']
+                        'SELECT lot, dlu, dlu_checkbox, nm_days, amazon, amazon_be FROM `' . _DB_PREFIX_ . 'product` WHERE id_product = ' . (int)$product['id_product']
                     );
                     
                     if ($result) {
@@ -381,6 +539,18 @@ class Champ_Personnalise_Produit extends Module
                         
                         if (isset($result['dlu_checkbox'])) {
                             $product['dlu_checkbox'] = $result['dlu_checkbox'];
+                        }
+                        
+                        if (isset($result['nm_days'])) {
+                            $product['nm_days'] = $result['nm_days'];
+                        }
+                        
+                        if (isset($result['amazon'])) {
+                            $product['amazon'] = $result['amazon'];
+                        }
+                        
+                        if (isset($result['amazon_be'])) {
+                            $product['amazon_be'] = $result['amazon_be'];
                         }
                     }
                 }
